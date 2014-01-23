32b4cd9c2828ddf7855ecdfa12197b68c20ec6ca
Gildas Le Louarn <gildas.le.louarn@gmail.com>
2014-01-23 16:25:52 +0100

    Heroku integration

--- external_api/settings_heroku.py
+++ external_api/settings_heroku.py
@@ -62,18 +62,7 @@ WSGI_APPLICATION = 'external_api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
-DATABASES = {
-#    'default': {
-#        'ENGINE':'django.db.backends.postgresql_psycopg2',
-#        'NAME': 'external_api',
-#        'USER': 'postgres',
-#        'PASSWORD': 'root',
-#        'HOST': '127.0.0.1',
-#        'PORT': '5432',
-#    },
-    'default': dj_database_url.config()
-
-}
+DATABASES['default'] =  dj_database_url.config()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
